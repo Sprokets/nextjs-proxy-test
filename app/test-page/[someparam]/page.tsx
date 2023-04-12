7da738e5b511a92791f17f587abd560b33ffbdfe
Dan Ott <dan@dtott.com>
2023-04-12 16:14:16 -0400

    add params

--- app/test-page/[someparam]/page.tsx
+++ app/test-page/[someparam]/page.tsx
@@ -1,9 +1,9 @@
 import NextLink from 'next/link';
 
-export default function Home() {
+export default function Page({ params }: { params: { someparam: string } }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24">
-      <h1 className="text-6xl font-bold">Test Page</h1>
+      <h1 className="text-6xl font-bold">Some Param: {params.someparam}</h1>
 
       <ul className="text-2xl font-semibold">
         <li>
